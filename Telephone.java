2ecee7690b39055598d9468d399b4cc6fc01b78c
anmcmillan <anmcmillan@oucs1423.otago.ac.nz>
2018-01-16 10:45:31 +1300

    Very Little Acheived

---
+++ Telephone.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+
+
+public static class Telephone{
+
+
+    public String number;
+    private boolean isValid = true;
+    
+
+
+   
+    public static void main(String args[]){
+
+        System.out.println("Enter a number");
+
+    }
+
+}
